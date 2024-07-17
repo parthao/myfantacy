50b908fd4a41245352a03f9ee24b87aea2f479d3
VishwaTechnologiesPvtLtd <151992843+VishwaTechnologiesPvtLtd@users.noreply.github.com>
2024-07-17 21:36:44 +0530

    dsfsdf

--- src/http-user.js
+++ src/http-user.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://122.169.42.21:8087",
+  baseURL: "http://122.169.42.21:8088",
   headers: {
     "Content-Type": "application/json",
   },
