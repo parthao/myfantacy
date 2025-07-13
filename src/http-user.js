47518523fec3423e346f2e35d24aeb2768b974fd
VishwaTechnologiesPvtLtd <151992843+VishwaTechnologiesPvtLtd@users.noreply.github.com>
2025-07-13 13:05:05 +0530

    changes in base url

--- src/http-user.js
+++ src/http-user.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://122.170.253.121:8088",
+  baseURL: "http://122.169.42.21:8088",
   headers: {
     "Content-Type": "application/json",
   },
